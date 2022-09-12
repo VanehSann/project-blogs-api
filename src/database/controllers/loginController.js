303dc057d30e7120499534e8e19e6ab98a53774f
VanehSann <vanessacbsantos22@gmail.com>
2022-09-12 02:30:21 -0300

    refactor: remove trybe files

--- src/database/controllers/loginController.js
+++ src/database/controllers/loginController.js
@@ -3,7 +3,7 @@ const tokenGenerate = require('../utils/token');
 const loginController = async (request, response) => {
 const { email } = request.body;
 
-  const token = tokenGenerate(email);
+  const token = tokenGenerate(request.email);
   response.status(200).json(token);
 };
 
